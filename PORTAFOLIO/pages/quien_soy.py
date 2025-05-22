470520185aa74182e47f69a19ec8bcfa8312db60
Celot1979 <zata_k@hotmail.com>
2025-05-22 10:42:27 +0200

    Creado el contenido de quien soy

--- PORTAFOLIO/pages/quien_soy.py
+++ PORTAFOLIO/pages/quien_soy.py
@@ -7,21 +7,25 @@ def quien_soy():
             # Contenedor de la imagen
             rx.box(
                 rx.image(
-                    src="/mi_foto.jpg",  # Coloca tu imagen en la carpeta assets
-                    width="300px",
-                    height="300px",
+                    src="https://i.ibb.co/q3MnTrT9/Portafolio-yo.jpg",
+                    width="600px",
+                    height="900px",
                     object_fit="cover",
                     border_radius="10px",
+                    alt="Foto de perfil de Daniel",
                 ),
                 margin_right="2em",
             ),
             # Contenedor de la descripción
             rx.box(
                 rx.text(
-                    "Aquí puedes escribir tu descripción personal. Cuéntanos sobre ti, tus intereses, "
-                    "tu experiencia y tus objetivos profesionales. Esta sección es una oportunidad "
-                    "para que los visitantes conozcan más sobre quién eres y qué te apasiona.",
-                    font_size="1.1em",
+                    """Mi nombre es Daniel. Al contrario que la mayoría de desarrolladores, encontré la tecnológia en mi fase madura.
+                    Considero que es un punto de valor al considerar que puedo aportar la ilusión y el hambre de quién descubre la programación,
+                    y la madurez de quién tiene paciencia para obtener resultados. Mi formación se ha realizado tanto en cursos guiados como autodidacta.
+                    Estaría encantado de participar  proyectos a los que unirme y desplegar todas las habilidades que he ido obteniendo.""",
+                    font_size="18px",
+                    font_weight="bold",
+                    font_family="'Poppins', sans-serif",
                     line_height="1.6",
                 ),
                 max_width="600px",
