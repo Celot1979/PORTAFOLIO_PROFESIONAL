d90dda3b6bff250f4560239bfd476b69ee007c35
Celot1979 <zata_k@hotmail.com>
2025-06-05 20:13:38 +0200

    Cambios para tener base de datos en local en postgreSQL

--- rxconfig.py
+++ rxconfig.py
@@ -1,9 +1,13 @@
 import reflex as rx
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 config = rx.Config(
     app_name="PORTAFOLIO",
     env=rx.Env.DEV,
-    db_url="sqlite:///reflex.db",
+    db_url=os.getenv("DATABASE_URL"),
     frontend_port=3000,
     backend_port=8000
 )
